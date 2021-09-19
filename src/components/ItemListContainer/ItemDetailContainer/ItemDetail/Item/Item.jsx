70bf97f709e337873bf32f09be96bdcc2580f2cf
belen duhart <mbelenduhart@gmail.com>
2021-09-19 20:20:44 -0300

    xx

--- src/components/ItemListContainer/ItemDetailContainer/ItemDetail/Item/Item.jsx
+++ src/components/ItemListContainer/ItemDetailContainer/ItemDetail/Item/Item.jsx
@@ -5,16 +5,6 @@ import { useEffect, useState } from "react";
 import ObtenerTarjetas from "../../../Mock/Mock";
 
 const Item = ({producto})=>{
-    // const ObtenerId = new Promise ((res,rej)=>{
-    //     let respuesta2 = "obtener";
-    //     if (respuesta2 === "obtener"){
-    //         setTimeout(()=>{
-    //             res({producto})
-    //         }, 1000)
-    //     }else {
-    //         rej("No se encuentran el producto")
-    //     }
-    // })
     const [productoComprar, setProductoComprar] = useState({});
     const {id}=useParams();
     
@@ -34,12 +24,7 @@ const Item = ({producto})=>{
 
     return (
         <>
-        {/* <div className="cardStyle">
-            <img className="cardImage" src={producto.picture} alt={producto.name} />
-            <p className="tituloProducto">{producto.name}</p> */}
         <Counter producto ={productoComprar}/>
-        {/* <button className="buttonStyle" style= {{width:"13vw", margin:"1vh 1vw"}} onClick = {AgregarAlCarrito} >¡Agregar al Carrito!</button>
-        </div> */}
         </>
     )
 
